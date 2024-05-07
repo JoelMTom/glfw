a77a3daa7bb9cc040d2ae6d217702e89ac503b2c
JoelMTom <joelqwerty007@gmail.com>
2024-05-07 17:31:32 +0530

    update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -19,99 +19,26 @@ project (projectName)
   Linklib[projectName] = projectName
 
   filter "system:linux"
-    defines "_GLFW_X11"
     defines "_GNU_SOURCE"
 
     removefiles {
       "src/cocoa*",
       "src/win32*",
-      "src/wl*",
+      "src/wgl*"
       -- "src/cocoa_time.c",
     }
 
+  filter "options:display-server=x11"
+    defines "_GLFW_X11"
+    removefiles {
+      "src/wl*",
+    }
 
---[[ project "GLFW"
-	kind "StaticLib"
-	language "C"
-	staticruntime "off"
+  filter "options:display-server=wayland"
+    defines "_GLFW_WAYLAND"
+    removefiles {
+      "src/x11*"
+    }
 
-	targetdir ("bin/" .. Outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. Outputdir .. "/%{prj.name}")
+  filter {}
 
-	files
-	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-		"src/platform.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
-	}
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_module.c",
-			"src/win32_window.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-		links
-		{
-			"Dwmapi.lib"
-		}
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on" ]]
